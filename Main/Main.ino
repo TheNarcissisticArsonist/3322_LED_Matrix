c7357756661b62209b00530155f74c8dd763c9a3
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2014-12-08 18:54:15 -0500

    Improved Framework

--- Main/Main.ino
+++ Main/Main.ino
@@ -9,8 +9,20 @@
 #define C A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
-int drawing;
-int numDrawings; //Define this as the number of permutations it can do.
+char drawing;
+char numDrawings; //Define this as the number of permutations it can do.
+
+void drawPicture16C(char row, char col, char picture, char width, char height) {
+  switch(picture) {
+    case 1:
+      //Iterate through picture 1
+      break;
+    case 2:
+      //Iterate through picture 2
+      break;
+    //etc...
+  }
+}
 
 void setup() {
   matrix.begin();
@@ -20,10 +32,10 @@ void setup() {
 }
 
 void loop() {
-  drawing = random(1, numDrawings + 1);
+  drawing = (char)random(1, numDrawings + 1);
   switch(drawing) {
     case 1:
-
+      
       break;
     case 2:
 
