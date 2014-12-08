6fb6ed1d5d0cc48aff187453b3ff418f7b49019e
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2014-12-08 18:42:19 -0500

    Setup to draw a random option

--- Main/Main.ino
+++ Main/Main.ino
@@ -9,10 +9,32 @@
 #define C A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
+int drawing;
+int numDrawings; //Define this as the number of permutations it can do.
+
 void setup() {
   matrix.begin();
+  //randomSeed(analogRead(5));
+  //The above should create a random sequence every time.
+  //It's not really necessary, but it could be useful.
 }
 
 void loop() {
-  
+  drawing = random(1, numDrawings + 1);
+  switch(drawing) {
+    case 1:
+
+      break;
+    case 2:
+
+      break;
+    //...
+    //...
+    /*...
+    case n:
+
+      break;
+
+    Where n is the number of drawings*/
+  }
 }
