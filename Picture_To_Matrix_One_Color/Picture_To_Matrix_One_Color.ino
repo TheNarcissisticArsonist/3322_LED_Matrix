9c9f72786523e73aef3d5f27b5f38bd404fa2305
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2014-12-11 20:07:59 -0500

    Separated Functions into Own Files

--- Picture_To_Matrix_One_Color/Picture_To_Matrix_One_Color.ino
+++ Picture_To_Matrix_One_Color/Picture_To_Matrix_One_Color.ino
@@ -11,17 +11,19 @@
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
 // | Insert the matrix from convertPictureToCode.py here
+// | The variable name used in this program is picture[][][], but you can change it.
 /* V
 
 */
 
 //Change picture[][ ][3] below to the number of columns in the drawing. Leave the first one blank!
-void drawPicture(unsigned char picture[][ ][3], int row, int col, int width, int height) {
+void drawPictureSingleColor(unsigned char picture[][ ][3], int row, int col, int width, int height, int colorR, int colorG, int colorB) {
   matrix.fillScreen(matrix.Color333(0, 0, 0));
+  unsigned short color = matrix.Color333(colorR, colorG, colorB);
   for(int i=0; i<height; i++) {
-    for(int j=0; j<width; j++) {
+    for(int j=0; j<height; j++) {
       if(picture[i][j][0] > 0 && picture[i][j][1] > 0 && picture[i][j][2] > 0) {
-        matrix.drawPixel(col+j, row+i, matrix.Color333(picture[i][j][0], picture[i][j][1], picture[i][j][2]));
+        matrix.drawPixel(col+j, row+i, color);
       }
     }
   }
@@ -30,7 +32,7 @@ void drawPicture(unsigned char picture[][ ][3], int row, int col, int width, int
 void setup() {
   matrix.begin();
   //Insert arguments based off variable names and the function definition above
-  drawPicture( , 0, 0, , );
+  drawPictureSingleColor( , 0, 0, , );
 }
 void loop() {
 
