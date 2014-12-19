2223e14e9acd11afd3340658fab2936afc26807b
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2014-12-18 19:13:47 -0500

    Created Main Function

---
+++ Main/Main.ino
@@ -0,0 +1,10 @@
+#include <Adafruit_GFX.h>
+#include <RGBmatrixPanel.h>
+
+#define CLK 8
+#define LAT A3
+#define OE 9
+#define A A0
+#define B A1
+#define C A2
+RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
