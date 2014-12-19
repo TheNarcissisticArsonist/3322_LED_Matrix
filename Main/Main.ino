5005b1a55a5a6603b88ee0800e101693d24d215d
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2014-12-18 19:19:23 -0500

    Added color variables

--- Main/Main.ino
+++ Main/Main.ino
@@ -9,6 +9,14 @@
 #define C A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
+unsigned short rB = matrix.Color333(0, 0, 7);
+unsigned short dB = matrix.Color333(0, 0, 1);
+unsigned short sB = matrix.Color333(0, 2, 7);
+unsigned short yC = matrix.Color333(7, 7, 0);
+unsigned short rC = matrix.Color333(7, 2, 0);
+unsigned short wC = matrix.Color333(7, 7, 7);
+
+
 
 void setup() {
   matrix.begin();
