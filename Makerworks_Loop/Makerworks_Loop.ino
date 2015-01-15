6bac6a472329293fd5301882dfcb1f68ec93b137
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-01-15 08:15:19 -0500

    MAKER

--- Makerworks_Loop/Makerworks_Loop.ino
+++ Makerworks_Loop/Makerworks_Loop.ino
@@ -11,8 +11,8 @@
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
 unsigned short b  = matrix.Color333(0, 0, 0); //Black
-unsigned short sB = matrix.Color333(0, 2, 7); //Skyline Blue
-unsigned short rB = matrix.Color333(0, 0, 7); //Regular Blue
+unsigned short s = matrix.Color333(0, 2, 7); //Skyline Blue
+unsigned short j = matrix.Color333(0, 0, 7); //Just Blue
 unsigned short g  = matrix.Color333(4, 7, 0); //Green
 unsigned short o  = matrix.Color333(7, 5, 0); //Orange
 unsigned short w  = matrix.Color333(7, 7, 7); //White
@@ -21,20 +21,20 @@ unsigned short y  = matrix.Color333(7, 7, 0); //Yellow
 unsigned short r  = matrix.Color333(7, 0, 0); //Red
 
 //M
-unsigned short c1[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
-unsigned short c2[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
-unsigned short c3[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
-unsigned short c4[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
-unsigned short c5[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
-unsigned short c6[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
-unsigned short c7[16] = {b, b, b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b};
-unsigned short c8[16] = {b, b, b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b};
-unsigned short c9[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
-unsigned short c10[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
-unsigned short c11[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
-unsigned short c12[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
-unsigned short c13[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
-unsigned short c14[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c1[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c2[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c3[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c4[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c5[16] = {b, b, s, s, s, s, s, s, b, b, b, b, b, b, b, b};
+unsigned short c6[16] = {b, b, s, s, s, s, s, s, b, b, b, b, b, b, b, b};
+unsigned short c7[16] = {b, b, b, b, s, s, s, s, s, s, b, b, b, b, b, b};
+unsigned short c8[16] = {b, b, b, b, s, s, s, s, s, s, b, b, b, b, b, b};
+unsigned short c9[16] = {b, b, s, s, s, s, s, s, b, b, b, b, b, b, b, b};
+unsigned short c10[16] = {b, b, s, s, s, s, s, s, b, b, b, b, b, b, b, b};
+unsigned short c11[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c12[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c13[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c14[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
 
 unsigned short c15[16] = {b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b};
 
@@ -79,6 +79,21 @@ unsigned short c46[16] = {o, o, b, b, b, b, b, b, b, b, b, b, b, b, o, o};
 unsigned short c47[16] = {o, o, b, b, b, b, b, b, b, b, b, b, b, b, o, o};
 
 unsigned short c48[16] = {b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b};
+
+//R
+unsigned short c49[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c50[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c51[16] = {s, s, s, s, s, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c52[16] = {s, s, b, b, b, b, s, s, b, b, b, b, b, b, b, b};
+unsigned short c53[16] = {s, s, b, b, b, b, s, s, b, b, b, b, b, b, b, b};
+unsigned short c54[16] = {s, s, b, b, b, b, s, s, b, b, b, b, b, b, b, b};
+unsigned short c55[16] = {s, s, b, b, b, b, s, s, s, b, b, b, b, b, b, b};
+unsigned short c56[16] = {s, s, b, b, b, b, s, s, s, s, b, b, b, b, b, b};
+unsigned short c57[16] = {s, s, s, b, b, s, s, s, s, s, s, s, s, s, s, s};
+unsigned short c58[16] = {b, s, s, s, s, s, s, b, s, s, s, s, s, s, s, s};
+unsigned short c59[16] = {b, b, s, s, s, s, b, b, b, s, s, s, s, s, s, s};
+
+unsigned short c60[16] = {b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b};
 
 
 void setup() {
