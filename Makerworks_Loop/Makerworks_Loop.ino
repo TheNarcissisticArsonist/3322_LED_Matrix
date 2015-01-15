ff5058d681df0c1a9a1c6b02fbd94fc47f23b1a7
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-01-14 20:54:59 -0500

    M

--- Makerworks_Loop/Makerworks_Loop.ino
+++ Makerworks_Loop/Makerworks_Loop.ino
@@ -18,6 +18,26 @@ unsigned short o  = matrix.Color333(7, 5, 0); //Orange
 unsigned short w  = matrix.Color333(7, 7, 7); //White
 unsigned short p  = matrix.Color333(7, 0, 7); //Purple
 unsigned short y  = matrix.Color333(7, 7, 0); //Yellow
+unsigned short r  = matrix.Color333(7, 0, 0); //Red
+
+unsigned short c1[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c2[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c3[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c4[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c5[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
+unsigned short c6[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
+unsigned short c7[16] = {b, b, b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b};
+unsigned short c8[16] = {b, b, b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b};
+unsigned short c9[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
+unsigned short c10[16] = {b, b, sB, sB, sB, sB, sB, sB, b, b, b, b, b, b, b, b};
+unsigned short c11[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c12[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c13[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+unsigned short c14[16] = {sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB, sB};
+
+unsigned short c15[16] = {b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b};
+
+
 
 void setup() {
   matrix.begin();
