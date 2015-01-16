12486601a73e1e79854c8ef1cd9abe285f1ee095
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-01-16 14:37:59 -0500

    Now Loops Continuously

--- Convert Binary to Short.js
+++ Convert Binary to Short.js
@@ -1,7 +1,8 @@
-
-var num = prompt("What number would you like to convert from binary to unsigned short?");
-var final = 0;
-for(i=0; i<16; i++) {
-  final += Number(num[i]) * Math.pow(2, 15-i);
+while(true) {
+  var num = prompt("What number would you like to convert from binary to unsigned short?");
+  var final = 0;
+  for(i=0; i<16; i++) {
+    final += Number(num[i]) * Math.pow(2, 15-i);
+  }
+  alert(final);
 }
-alert(final);
