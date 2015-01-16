0fac54b7e2c86e8f412941b75f20d0356d2386e2
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-01-16 08:48:54 -0500

    Works without internet now

--- Convert Binary to Short.js
+++ Convert Binary to Short.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
-  var num = prompt("What number would you like to convert from binary to unsigned short?");
-  var final = 0;
-  for(i=0; i<16; i++) {
-    final += Number(num[i]) * Math.pow(2, 15-i);
-  }
-  alert(final);
-});
+
+var num = prompt("What number would you like to convert from binary to unsigned short?");
+var final = 0;
+for(i=0; i<16; i++) {
+  final += Number(num[i]) * Math.pow(2, 15-i);
+}
+alert(final);
