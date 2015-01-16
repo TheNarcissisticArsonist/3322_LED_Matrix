b1bf1e314de79de0739ee26b4afa71b9f2c92d94
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-01-15 20:13:00 -0500

    Made my method better

--- Convert Binary to Short.js
+++ Convert Binary to Short.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var num = prompt("What number would you like to convert from binary to unsigned short?");
   var final = 0;
   for(i=0; i<16; i++) {
-    final += Number(num[i]) * Math.pow(2, 16-i);
+    final += Number(num[i]) * Math.pow(2, 15-i);
   }
-  alert(final/2);
+  alert(final);
 });
