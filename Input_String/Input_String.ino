dd2bf5d4562ec351c6bbd71aa20d6e6d27aec154
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-01-23 18:40:25 -0500

    Setup

---
+++ Input_String/Input_String.ino
@@ -0,0 +1,18 @@
+#include <Adafruit_GFX.h>
+#include <RGBmatrixPanel.h>
+
+
+#define CLK 8
+#define LAT A3
+#define OE 9
+#define A A0
+#define B A1
+#define C A2
+RGBmatrixPanel Matrix(A, B, C, CLK, LAT, OE, false);
+
+void setup() {
+  Matrix.begin();
+}
+void loop() {
+  
+}
